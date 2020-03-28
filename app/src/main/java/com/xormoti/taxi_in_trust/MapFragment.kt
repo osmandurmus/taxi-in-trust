a32b24a47ee08a498f7a30601cfd14674cecd4e8
vahitdurmus <vahitdurmuss@gmail.com>
2020-03-28 20:15:39 +0300

    added mapbox

--- app/src/main/java/com/xormoti/taxi_in_trust/MapFragment.kt
+++ app/src/main/java/com/xormoti/taxi_in_trust/MapFragment.kt
@@ -1,54 +1,67 @@
 package com.xormoti.taxi_in_trust
 
-import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-
-
-
-
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.Style
 
 class MapFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    private var listener: OnFragmentInteractionListener? = null
-
+    private lateinit var mapView: MapView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        context?.let { Mapbox.getInstance(it, getString(R.string.mapbox_access_token)) };
     }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false)
+        val view= inflater.inflate(R.layout.fragment_map, container, false)
+        mapView= view.findViewById<MapView>(R.id.mapView);
+        return view
     }
-
-
-
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView?.onCreate(savedInstanceState)
+        mapView?.getMapAsync { mapboxMap ->
+            mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+            }
+        }
+    }
     interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         fun onFragmentInteraction(uri: Uri)
     }
-
-
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStart()
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mapView?.onDestroy()
+    }
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        if (outState != null) {
+           mapView.onSaveInstanceState(outState)
+        }
+    }
 }
