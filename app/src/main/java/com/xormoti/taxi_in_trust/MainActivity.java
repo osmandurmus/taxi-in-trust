39fac5c2e27ea8d4006a53de8c28887bc127c588
OSMAN DURMUS <osmandurmus4@gmail.com>
2020-04-11 16:45:52 +0300

    Fragmentler java class olarak değiştirildi. Navigation kaldırıldı.

---
+++ app/src/main/java/com/xormoti/taxi_in_trust/MainActivity.java
@@ -0,0 +1,14 @@
+package com.xormoti.taxi_in_trust;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
