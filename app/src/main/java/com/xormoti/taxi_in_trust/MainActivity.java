67f394a8c364495d6311b4137ec1269d68523b0e
vahitdurmus <vahitdurmuss@gmail.com>
2020-05-12 00:39:23 +0300

    auth

--- app/src/main/java/com/xormoti/taxi_in_trust/MainActivity.java
+++ app/src/main/java/com/xormoti/taxi_in_trust/MainActivity.java
@@ -1,8 +1,22 @@
 package com.xormoti.taxi_in_trust;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +24,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
--- app/src/main/java/com/xormoti/taxi_in_trust/ProfileFragment.java
+++ app/src/main/java/com/xormoti/taxi_in_trust/ProfileFragment.java
@@ -7,7 +7,6 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,8 +16,6 @@ public class ProfileFragment extends Fragment {
     public ProfileFragment() {
         // Required empty public constructor
     }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
